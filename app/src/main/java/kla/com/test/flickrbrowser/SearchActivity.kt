8ada064f306d9af3af26e6464f0c0f2fc068a9ca
Kevin La <chenchiaho@gmail.com>
2019-12-23 09:30:37 +0700

    Initial commit

---
+++ app/src/main/java/kla/com/test/flickrbrowser/SearchActivity.kt
@@ -0,0 +1,48 @@
+package kla.com.test.flickrbrowser
+
+import android.app.SearchManager
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
+import javax.xml.transform.Templates
+
+class SearchActivity : BaseActivity() {
+    private val TAG = "SearchActivity"
+    private var searchView: SearchView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, ".onCreate: starts")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        activateToolbar(true)
+        Log.d(TAG, ".onCreate: ends")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        Log.d(TAG, ".onCreateOptionsMenu: starts")
+        menuInflater.inflate(R.menu.menu_search, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView = menu.findItem(R.id.app_bar_search).actionView as SearchView
+        val searchableInfo = searchManager.getSearchableInfo(componentName)
+        searchView?.setSearchableInfo(searchableInfo)
+        searchView?.isIconified = false
+
+        searchView?.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d(TAG, ".onQueryTextSubmit: called")
+
+                finish()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        Log.d(TAG, ".onCreateOptionsMenu: returning")
+        return true
+    }
+}
